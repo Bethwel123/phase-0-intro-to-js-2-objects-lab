75ad1c306a634dd12ad95b106de24b99fc1e2b4e
Bethwel123 <bethwellangat654@gmail.com>
2024-06-22 18:39:03 +0300

    Created objects in index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const obj = Object.assign({}, employee);
+    delete obj[key];
+    return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+}
